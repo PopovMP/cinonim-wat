67f32b0403d6d8649ff2eaa12cbc6d2fc6770423
Miroslav Popov <info@forexsb.com>
2022-09-18 21:38:37 +0000

    Compile global vars and const.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {NodeType}  = require("@popovmp/cinonim-parser")
+const {NodeType, DataType} = require("@popovmp/cinonim-parser")
 
 /**
  * Compile Cinonim AST to WAT
@@ -13,29 +13,85 @@ function astToWat(moduleNode)
 {
 	const output = []
 
-	walkAst(moduleNode, output)
+	walkAst(moduleNode, output, 0)
 
 	return output.join('')
 }
 
 /**
  *
- * @param {Node} parentNode
+ * @param {Node} node
  * @param {string[]} output
+ * @param {number} depth
  */
-function walkAst(parentNode, output)
+function walkAst(node, output, depth)
 {
-
-	if (parentNode.type === NodeType.module) {
+	// Module
+	if (node.type === NodeType.module) {
 		output.push('(module')
 
-		for (const node of parentNode.nodes)
-			walkAst(node, output)
+		if (node.nodes.length > 0) {
+			output.push('\n')
+
+			for (const child of node.nodes)
+				walkAst(child, output, depth + 1)
+		}
 
 		output.push(')')
 	}
+
+	// Global var
+	if (node.type === NodeType.globalVar) {
+		const name     = node.value
+		const dataType = getDataType(node)
+		const value    = node.nodes[0].value
+
+		const wat = `(global $${name} (mut ${dataType}) (${dataType}.const ${value}))`
+		output.push( lpad(wat, depth) )
+	}
+
+	// Global const
+	if (node.type === NodeType.globalConst) {
+		const name     = node.value
+		const dataType = getDataType(node)
+		const value    = node.nodes[0].value
+
+		const wat = `(global $${name} ${dataType} (${dataType}.const ${value}))`
+
+		output.push( lpad(wat, depth) )
+	}
+
 }
 
+/**
+ * Gets data type
+ * @param {Node} node
+ * @return {string}
+ */
+function getDataType(node)
+{
+	switch (node.dataType) {
+		case DataType.f32: return 'f32'
+		case DataType.f64: return 'f64'
+		case DataType.i32: return 'i32'
+		case DataType.i64: return 'i64'
+	}
+
+	throw new Error('Unknown data type: ' + node.dataType)
+}
+
+/**
+ * Gets left pad
+ *
+ * @param {string} wat
+ * @param {number} depth
+ *
+ * @return {string}
+ */
+function lpad(wat, depth)
+{
+	return '    '.repeat(depth) + wat + '\n'
+}
 
 
 module.exports = {
