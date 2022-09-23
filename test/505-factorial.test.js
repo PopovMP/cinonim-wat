46c8913aad2eac351a56c26811779f110455a6c8
Miroslav Popov <info@forexsb.com>
2022-09-23 20:48:15 +0000

    Added fibo test.

---
+++ test/505-factorial.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const {strictEqual} = require('assert')
+
+const {tokenize, clean} = require('@popovmp/tokenizer')
+const {parse}           = require('@popovmp/cinonim-parser')
+const {describe, it}    = require('@popovmp/mocha-tiny')
+
+const {astToWat} = require('../index.js')
+
+const src = `
+#export-func factorial = factorial
+
+long factorial(int n)
+{
+	if (n == 0) { return 1; }
+	return ((long) n) * factorial(n - 1);
+}
+`
+
+const expected = `
+(module
+    (export "factorial" (func $factorial))
+    (func $factorial (param $n i32) (result i64)
+        (local.get $n) (i32.const 0) (i32.eq)
+        (if (then
+            (i64.const 1)
+            (return)
+        ))
+        (local.get $n) (i64.extend_i32_s) (local.get $n) (i32.const 1) (i32.sub)
+        (call $factorial) (i64.mul)
+    )
+)
+`
+
+describe('factorial', () => {
+	it('compiles factorial to WAT', () => {
+		const actual = '\n' + astToWat(parse(clean(tokenize(src)))) + '\n'
+		strictEqual(actual, expected)
+	})
+})
